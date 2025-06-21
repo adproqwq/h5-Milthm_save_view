cc6316d3851013ae051cd9708f4a7adcea55537f
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-06-22 03:40:50 +0800

    feat: Telegram 交流群

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,9 @@
 <template>
   <mdui-card variant="outlined" class="box">
     <div class="insideBox">
-      <mdui-button variant="filled" href="https://qm.qq.com/q/Utb6sNDvki" target="_blank">QQ交流群</mdui-button>
-      <mdui-button variant="filled" href="https://github.com/adproqwq/h5-Milthm_save_view" target="_blank">Github仓库</mdui-button>
+      <mdui-button variant="filled" href="https://qm.qq.com/q/Utb6sNDvki" target="_blank">QQ 交流群</mdui-button>
+      <mdui-button variant="filled" href="https://t.me/+PeJuV_ZFzQQxM2E1" target="_blank">Telegram 交流群</mdui-button>
+      <mdui-button variant="filled" href="https://github.com/adproqwq/h5-Milthm_save_view" target="_blank">Github 仓库</mdui-button>
     </div>
   </mdui-card>
 </template>
