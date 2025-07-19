d83111840109d4c0a88148d01084fcb64555eacc
adproqwq <adpro_qwq@outlook.com>
2025-07-19 17:32:29 +0800

    feat: Codeberg 仓库

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,6 +3,7 @@
     <div class="insideBox">
       <mdui-button variant="filled" href="https://qm.qq.com/q/Utb6sNDvki" target="_blank">QQ 交流群</mdui-button>
       <mdui-button variant="filled" href="https://t.me/+PeJuV_ZFzQQxM2E1" target="_blank">Telegram 交流群</mdui-button>
+      <mdui-button variant="filled" href="https://codeberg.org/adproqwq/MilthmSaveExplorer" target="_blank">Codeberg 仓库</mdui-button>
       <mdui-button variant="filled" href="https://github.com/adproqwq/h5-Milthm_save_view" target="_blank">Github 仓库</mdui-button>
     </div>
   </mdui-card>
